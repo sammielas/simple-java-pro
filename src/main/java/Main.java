d0c80facbfd464fb48fb1a26f7da5473a4f64332
Samson Lasode <samflicks@gmail.com>
2025-02-25 11:16:07 +0000

    modify main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 // src/main/java/Main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins from here!");
+        System.out.println("Hello, Jenkins!!!!");
     }
 
     public static int add(int a, int b) {
