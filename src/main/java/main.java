73f2b9e12d8395fe2a3a84fb739e852cce888816
Samson Lasode <samflicks@gmail.com>
2025-02-25 10:45:24 +0000

    reorder java starter files

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+// src/main/java/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
